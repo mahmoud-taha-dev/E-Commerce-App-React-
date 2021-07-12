7963d62ea22d3bc6b66ac19e5d95cd30222f5283
Mahmoud Taha <mahmoud.taha.dev@gmail.com>
2021-07-13 00:36:04 +0200

    page not found

--- src/containers/NotFound/NotFound.jsx
+++ src/containers/NotFound/NotFound.jsx
@@ -2,9 +2,7 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 const NotFound = () => {
   return (
     <PageTitle title="Page Not Found">
-      <div className="container">
-        <h1 className="text-center">Page Not Found</h1>
-      </div>
+      <h1 className="text-center">Page Not Found</h1>
     </PageTitle>
   );
 };
